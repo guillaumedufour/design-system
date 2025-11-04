6884fe0842133f4e2694f59d6b85c71f3a988296
Guillaume Gdlab <guillaume@gdlab.fr>
2025-11-04 13:29:04 +0100

    add constants file

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {useFonts} from 'expo-font';
+import React from "react";
+import {colors} from "./constants/colors";
+import {radius} from "./constants/radius";
+import {padding} from "./constants/padding";
+import {margin} from "./constants/margin";
 
+
+const CARD_PADDING = 20;
 export default function App() {
-  return (
+
+  const [fontLoaded] = useFonts({
+    "Inter-Regular": require('./assets/fonts/Inter-Regular.ttf'),
+    "Inter-Bold": require('./assets/fonts/Inter-Bold.ttf'),
+  })
+  if (!fontLoaded) return null;
+  return fontLoaded ? (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto"/>
+      <View style={[styles.card, {backgroundColor: colors.VIOLET}]}>
+        <View style={styles.cardContent}/>
+      </View>
+      <View style={[styles.card, {backgroundColor: colors.GREY}]}></View>
+      <View style={[styles.card, {backgroundColor: colors.PURPLE}]}></View>
+      <View style={[styles.card, {backgroundColor: colors.ORANGE}]}></View>
+      <View style={[styles.card, {backgroundColor: colors.DARK}]}></View>
     </View>
-  );
+  ) : null;
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +36,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: padding.HORIZONTAL_SCREEN
   },
+  card: {
+    width: '100%',
+    height: '15%',
+    borderWidth: 1,
+    marginVertical: margin.VERTICAL_SEPARATOR,
+    borderRadius: radius.MEDIUM,
+    padding: CARD_PADDING,
+  },
+  cardContent: {
+    width: '100%',
+    height: '100%',
+    borderRadius: radius.MEDIUM,
+    backgroundColor: colors.LIGHT
+  }
 });
